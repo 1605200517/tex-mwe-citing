aad0080278da52d2397869d7793a101b829eb0b5
J.Doe <Snail Mail>
2018-02-25 08:54:15 +0100

    demonstrating biber installation problem

--- miktex-mweb/compile-in-docker.sh
+++ miktex-mweb/compile-in-docker.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-apt -y update
-apt-get -y install biber
+#apt -y update
+#apt-get -y install biber
 
-pdflatex mweb-biblatex
-pdflatex mweb-biblatex
-echo "now running biber!"
-biber mweb-biblatex
-pdflatex mweb-biblatex
+
+#mpm --admin --verbose --package-level=complete --upgrade
+
+mpm --admin --verify
+mpm --admin --verbose --update
+mpm --admin --verbose --install biber
+
+#cat /root/.miktex/texmfs/data/miktex/log/mpmcli.log
+cat /var/log/miktex/mpmcli_admin.log
+#pdflatex mweb-biblatex
+#pdflatex mweb-biblatex
+#echo "now running biber!"
+#biber mweb-biblatex
+#pdflatex mweb-biblatex
